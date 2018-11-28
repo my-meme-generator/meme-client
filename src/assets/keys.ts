e73bf9dc52a1d274bdbe09638970f9b0ba77cd63
Cole Phares <cophares@pdx.edu>
2018-11-28 15:31:50 -0800

    sensitive data removed

--- src/assets/keys.ts
+++
@@ -1,7 +0,0 @@
-export class Keys {
-  templateAlbumHash: string = 'cY45kfx';
-  memeAlbumHash: string = '0hzHSAI';
-  clientId: string = 'a3e0a22324ca1fa';
-  clientSecret: string = 'c6abe70b0e994f2f06efc3de379f8da3558327c1';
-  accessToken: string = '35e9b3ff3e017b88e8772b9af1af5624b1d1e91a';
-}
